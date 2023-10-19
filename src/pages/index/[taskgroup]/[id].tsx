94cf54b314231e3580f78eb2d8cd07db74cde359
Arielle Gironza <ariellegironza@Arielles-MacBook-Air.local>
2023-10-18 21:19:10 -0400

    hope this works haha

--- src/pages/index/[taskgroup]/[id].tsx
+++
@@ -1,3 +0,0 @@
-// export default function SingleTask() {
-//     return <h1>Single Task</h1>
-// };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // colors: {
+      //   01: 
+
+      // }
+    },
   },
   plugins: [],
 } satisfies Config;
